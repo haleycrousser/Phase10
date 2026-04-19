bad4293dd3064f3b7d253f4d95fc48745ac9ac5f
Haley Crousser <haleycrousser99@gmail.com>
2026-04-19 15:23:20 -0500

    Revert "Whoops"
    This reverts commit 8b0e8918021550daa8294b0dea58c52724097ae5.

--- gameLoop/card.h
+++ gameLoop/card.h
@@ -2,21 +2,19 @@
 #define CARD_H
 
 #include <iostream>
-#include <string>
 
 class Card {
-
+    
 public:
-
-    std::string color;
+    string color;
     int    num;
     char   variant;
     int    points;
     bool   wild;
     bool   skip;
 
-    Card(std::string color, int num, char variant, int points, bool wild, bool skip);
-    static Card createCard(std::string color, int num, char variant, int points);
+    Card(string color, int num, char variant, int points, bool wild, bool skip);
+    static Card createCard(string color, int num, char variant, int points);
 };
 
 #endif
